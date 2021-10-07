692b8e4f157904a419b523490286b245c5a8d494
Juan S. Lozano <A01374452@itesm.mx>
2021-10-06 19:50:51 -0500

    Added Videogames Operations

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
+
+const vjRoutes = require('./routes/videojuegos')
+const sequelize = require('./utils/database')
+
+app.use("/videojuegos",vjRoutes);
+
 
 app.get("/test", (req, res) => {
     res.send("Server Test Succesful")
