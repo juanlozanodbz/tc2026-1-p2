5b3dee24d1b458501f4b2fb54670aa7f75628023
Juan S. Lozano <A01374452@itesm.mx>
2021-10-06 20:24:33 -0500

    Added mysql functionality

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get("/test", (req, res) => {
     res.send("Server Test Succesful")
 })
 
-app.listen(8083, () => {
-    console.log("Server online on port 8083")
-})
+sequelize.sync()
+    .then(()=>{
+        app.listen(8083,()=>{
+            console.log("Aplicación web en línea en el puerto 8083")
+        })
+    })
+    .catch(err=>console.log(err))
